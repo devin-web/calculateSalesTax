7f460edbf81f8ed5e2dfee05d61304302affdbc2
devin-web <devin@alumni.sfu.ca>
2016-09-07 03:54:25 +0000

    initial commit. Looks like it works

---
+++ calculateSalesTax.js
@@ -0,0 +1,63 @@
+var taxRates = {
+  AB: 0.05,
+  BC: 0.12,
+  SK: 0.10
+};
+
+var companySalesData = [
+  {
+    name: "Telus",
+    province: "BC",
+    sales: [ 100, 200, 400 ]
+  },
+  {
+    name: "Bombardier",
+    province: "AB",
+    sales: [ 80, 20, 10, 100, 90, 500 ]
+  },
+  {
+    name: "Telus",
+    province: "SK",
+    sales: [ 500, 100 ]
+  }
+];
+
+function sumSales( company ){
+  var sum = 0;
+  for( var i = 0; i < company.sales.length; i++ ){
+    sum += company.sales[i];
+  }
+  return sum;
+}
+
+function calculateSalesTax(sales, taxRates) {
+  newObj = {}
+
+  for( var i = 0; i < sales.length; i++ ){
+    var salesSum = sumSales(sales[i]);
+    if( newObj.hasOwnProperty( sales[i].name )) {
+      newObj[sales[i].name] = { totalSales: newObj[sales[i].name].totalSales + salesSum,
+                                totalTaxes: newObj[sales[i].name].totalTaxes + salesSum*taxRates[sales[i].province] };
+    }
+    else {
+      newObj[sales[i].name] = { totalSales: salesSum, totalTaxes: salesSum*taxRates[sales[i].province] };
+    }
+  }
+  return newObj;
+}
+
+var results = calculateSalesTax(companySalesData, taxRates);
+
+console.log( results );
+/* Expected Results:
+{
+  Telus: {
+    totalSales: 1300
+    totalTaxes: 144
+  },
+  Bombardier: {
+    totalSales: 800,
+    totalTaxes: 40
+  }
+}
+*/
